c0a31ad167d8e6cdba476328b94a332bb7f6bb8b
Christopher Abate <contact@christopherabate.com>
2015-11-23 19:25:03 +0100

    Unnecessary files deleted.

--- form.php
+++
@@ -1,9 +0,0 @@
-<form id="<?php echo $name ?> "action="<?php echo $action ?>" method="<?php echo $method ?>" enctype="<?php echo $enctype ?>">
-    <?php foreach ($fields as $field): ?>
-        <?php echo $field ?>
-    <?php endforeach ?>
-
-    <?php foreach ($buttons as $button): ?>
-        <?php echo $button ?>
-    <?php endforeach ?>
-</form>
